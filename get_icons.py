89618e6070d066e7b91238a066d6c66683e022d8
SHOOPIDOOP <85492626+SHOOPIDOOP@users.noreply.github.com>
2023-11-14 21:31:38 +0100

    Add files via upload

--- get_icons.py
+++ get_icons.py
@@ -2,11 +2,10 @@ import cv2
 import os
 import numpy as np
 import random
-path_to_icons = r"C:\Users\metal\Desktop\DATASETGENERATOR\icons"
-path_to_minimap = r"C:\Users\metal\Desktop\DATASETGENERATOR\minimap.PNG"
-blue_icons = r"C:\Users\metal\Desktop\DATASETGENERATOR\blue_icons"
-red_icons = r"C:\Users\metal\Desktop\DATASETGENERATOR\red_icons"
-
+path_to_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/icons"
+path_to_minimap = r"minimap.PNG"
+blue_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/blue_icons"
+red_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/red_icons"
 def get_icons(color=None):
     icons = []
     for p in os.listdir(path_to_icons):
@@ -18,23 +17,21 @@ def get_icons(color=None):
         if not os.path.isfile(p):
             continue
         
-        img = cv2.imread(p)
-
-        w, h = img.shape[:2]
+        img = cv2.imread(p, -1)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
+        w, h, c = img.shape
 
         c_w = w // 2
         c_h = h // 2
 
         radius = w // 2
-
         mask1 = np.zeros_like(img)
+        mask1 = cv2.circle(mask1, (c_w,c_h), radius, (0,0,0, 255), -1) 
+        print(mask1[:,:, 3]) 
 
-        mask1 = cv2.circle(mask1, (c_w,c_h), radius, (255,255,255), -1)
-
-        img = cv2.subtract(mask1, img)
-
-        img = cv2.bitwise_not(img)
-
+        img = cv2.subtract(img, mask1)
+        
+  
         if color == "red":
             red_outline = cv2.circle(img, (c_w, c_h), radius, (0,0,255),3)
             img = cv2.resize(img, (35, 35))
@@ -43,7 +40,7 @@ def get_icons(color=None):
         if color == "blue":
             blue_outline = cv2.circle(img, (c_w, c_h), radius, (255,0,0),3)
             img = cv2.resize(img, (35, 35))
-            cv2.imwrite(rf'C:\Users\metal\Desktop\DATASETGENERATOR\blue_icons\{champion}_blue.png', img)
+            cv2.imwrite(rf'{blue_icons}/{champion}_blue.png', img)
 
         if color == None:
             img = cv2.resize(img, (35, 35))
@@ -53,26 +50,30 @@ def get_icons(color=None):
 
     return icons
 
+icons = get_icons("blue")
+
 minimap = cv2.imread(path_to_minimap)
-total_champs = os.listdir(red_icons)
+minimap = cv2.cvtColor(minimap, cv2.COLOR_RGB2RGBA)
+total_champs = os.listdir(blue_icons)
 
-random_champ = random.randint(0, len(total_champs))
-y_offset = random.randint(0, minimap.shape[0])
-x_offset = random.randint(0, minimap.shape[1])
-rnd_champ_path = total_champs[random_champ]
-rnd_champ = cv2.imread(rf"C:\Users\metal\Desktop\DATASETGENERATOR\red_icons\{rnd_champ_path}")
-
-
-for j in range(5):
-    random_champ = random.randint(0, len(total_champs))
-    y_offset = random.randint(0, minimap.shape[0])
-    x_offset = random.randint(0, minimap.shape[1])
-    rnd_champ_path = total_champs[random_champ]
-    rnd_champ = cv2.imread(rf"C:\Users\metal\Desktop\DATASETGENERATOR\red_icons\{rnd_champ_path}")
-    minimap[y_offset:y_offset+rnd_champ.shape[0], x_offset:x_offset+rnd_champ.shape[1]] = rnd_champ
-
-
-
+for champion, icon in icons:
+    for i in range(5):
+        try:
+            y_offset = random.randint(0, minimap.shape[0])
+            x_offset = random.randint(0, minimap.shape[1])
+            y1,y2 = y_offset, y_offset + icon.shape[0]
+            x1,x2 = x_offset, x_offset + icon.shape[1]
+            #cv2.imshow("test", icon[:, :, 3])
+            #cv2.waitKey(0)
+            icon[:, :, 3] = 255.0
+            alpha_s = icon[:, :, 3] / 255.0
+            alpha_l = 1.0 - alpha_s
+            
+            for c in range(0,3):
+                minimap[y1:y2, x1:x2, c] = (alpha_s * icon[:, :, c] + alpha_l * minimap[y1:y2, x1:x2, c])
+        except:
+            print("", end="")
+    break
 cv2.imshow("data", minimap)
 cv2.waitKey(0)
 
