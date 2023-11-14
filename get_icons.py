18c8c0fad18ad882d1a23c7db753429cb78f4aaf
SHOOPIDOOP <85492626+SHOOPIDOOP@users.noreply.github.com>
2023-11-15 00:05:54 +0100

    Add files via upload

--- get_icons.py
+++ get_icons.py
@@ -2,10 +2,10 @@ import cv2
 import os
 import numpy as np
 import random
-path_to_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/icons"
+path_to_icons = r"C:\Users\metal\Desktop\latest\DATASETGENERATOR\icons\icons"
 path_to_minimap = r"minimap.PNG"
-blue_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/blue_icons"
-red_icons = r"/home/biggiecheese1235/DATASETGENERATION/GITREPO/DATASETGENERATOR/red_icons"
+blue_icons = r"C:\Users\metal\Desktop\latest\DATASETGENERATOR\blue_icons\blue_icons"
+red_icons = r"C:\Users\metal\Desktop\latest\DATASETGENERATOR\red_icons\red_icons"
 def get_icons(color=None):
     icons = []
     for p in os.listdir(path_to_icons):
@@ -26,19 +26,20 @@ def get_icons(color=None):
 
         radius = w // 2
         mask1 = np.zeros_like(img)
-        mask1 = cv2.circle(mask1, (c_w,c_h), radius, (0,0,0, 255), -1) 
-        print(mask1[:,:, 3]) 
+        mask1 = cv2.bitwise_not(mask1)
+        mask1 = cv2.cvtColor(mask1, cv2.COLOR_RGB2RGBA)
+        mask1 = cv2.circle(mask1, (c_w,c_h), radius, (0,0,0,0), -1)
 
         img = cv2.subtract(img, mask1)
         
   
         if color == "red":
-            red_outline = cv2.circle(img, (c_w, c_h), radius, (0,0,255),3)
+            img = cv2.circle(img, (c_w, c_h), radius, (100,100,255,255),3)
             img = cv2.resize(img, (35, 35))
             cv2.imwrite(rf'C:\Users\metal\Desktop\DATASETGENERATOR\red_icons\{champion}_red.png', img)
         
         if color == "blue":
-            blue_outline = cv2.circle(img, (c_w, c_h), radius, (255,0,0),3)
+            img = cv2.circle(img, (c_w, c_h), radius, (255, 170, 30, 255),4)
             img = cv2.resize(img, (35, 35))
             cv2.imwrite(rf'{blue_icons}/{champion}_blue.png', img)
 
@@ -50,30 +51,85 @@ def get_icons(color=None):
 
     return icons
 
-icons = get_icons("blue")
-
+icons_blue = get_icons("blue")
+icons_red = get_icons("red")
 minimap = cv2.imread(path_to_minimap)
 minimap = cv2.cvtColor(minimap, cv2.COLOR_RGB2RGBA)
-total_champs = os.listdir(blue_icons)
+minimap_base = cv2.imread(path_to_minimap)
+minimap_base = cv2.cvtColor(minimap, cv2.COLOR_RGB2RGBA)
+pings = os.listdir(r"C:\Users\metal\Desktop\latest\DATASETGENERATOR\out")
+total_champs = os.listdir(r"C:\Users\metal\Desktop\latest\DATASETGENERATOR\blue_icons\blue_icons")
 
-for champion, icon in icons:
-    for i in range(5):
-        try:
-            y_offset = random.randint(0, minimap.shape[0])
-            x_offset = random.randint(0, minimap.shape[1])
-            y1,y2 = y_offset, y_offset + icon.shape[0]
-            x1,x2 = x_offset, x_offset + icon.shape[1]
-            #cv2.imshow("test", icon[:, :, 3])
-            #cv2.waitKey(0)
-            icon[:, :, 3] = 255.0
-            alpha_s = icon[:, :, 3] / 255.0
-            alpha_l = 1.0 - alpha_s
-            
-            for c in range(0,3):
-                minimap[y1:y2, x1:x2, c] = (alpha_s * icon[:, :, c] + alpha_l * minimap[y1:y2, x1:x2, c])
-        except:
-            print("", end="")
-    break
+test = get_icons("red")
+new_file = open("labels.txt", "w+")
+bruh = len(test)
+i = 0
+for champion, icon in test:
+
+    new_file.write(f"{champion}\n")
+
+    i += 1
+    
+
+
+for j in range(1000):
+    if j%5 != 0:
+        f = open(rf"C:\Users\metal\Desktop\latest\DATASETGENERATOR\dataset\labels\train\frame_{j}.txt", "w+")
+    if j%5 == 0:
+        f = open(rf"C:\Users\metal\Desktop\latest\DATASETGENERATOR\dataset\labels\val\frame_{j}.txt", "w+")        
+    for champion, icon in icons_blue:
+        for i in range(5):
+            try:
+                r = random.randint(0, len(icons_blue))
+                random_champ = icons_blue[r][1]
+                y_offset = random.randint(20, minimap.shape[0]- 50)
+                x_offset = random.randint(20, minimap.shape[1]- 50)
+                y1,y2 = y_offset, y_offset + random_champ.shape[0]
+                x1,x2 = x_offset, x_offset + random_champ.shape[1]
+                alpha_s = random_champ[:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_s
+                #cv2.rectangle(minimap, (x_offset, y_offset), (x_offset+random_champ.shape[1], y_offset + random_champ.shape[0]), (255,255,255), 1)
+                f.write(f"{r} {x_offset / 415} {y_offset / 415} {(x_offset + random_champ.shape[1]) / 415} {(y_offset + random_champ.shape[0]) / 415}\n")
+                #cv2.putText(minimap, icons_red[r][0], (x_offset, y_offset-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+                for c in range(0,3):
+                    minimap[y1:y2, x1:x2, c] = (alpha_s * random_champ[:, :, c] + alpha_l * minimap[y1:y2, x1:x2, c])
+                    
+            except:
+                print("", end="")
+        break
+
+    
+    for champion, icon in icons_red:
+        for i in range(5):
+            try:
+                r = random.randint(0, len(icons_red))
+                random_champ = icons_red[r][1]
+                y_offset = random.randint(20, minimap.shape[0] - 50)
+                x_offset = random.randint(20, minimap.shape[1] - 50)
+                y1,y2 = y_offset, y_offset + random_champ.shape[0]
+                x1,x2 = x_offset, x_offset + random_champ.shape[1]
+                alpha_s = random_champ[:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_s
+                f.write(f"{r} {x_offset / 415} {y_offset / 415} {(x_offset + random_champ.shape[1]) / 415} {(y_offset + random_champ.shape[0]) / 415}\n")
+                #cv2.putText(minimap, icons_red[r][0], (x_offset, y_offset-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+                for c in range(0,3):
+                    minimap[y1:y2, x1:x2, c] = (alpha_s * random_champ[:, :, c] + alpha_l * minimap[y1:y2, x1:x2, c])
+                
+
+            except:
+                print("", end="")   
+        break
+
+    if (not (j%5) == 0):
+        cv2.imwrite(rf"C:\Users\metal\Desktop\latest\DATASETGENERATOR\dataset\images\train\frame_{j}.png",minimap)
+    if j % 5 == 0:
+        cv2.imwrite(rf"C:\Users\metal\Desktop\latest\DATASETGENERATOR\dataset\images\val\frame_{j}.png",minimap)
+    minimap = cv2.imread(path_to_minimap)
+    f.close()
+    
+
+    
+minimap = cv2.resize(minimap, (415, 415))
 cv2.imshow("data", minimap)
 cv2.waitKey(0)
 
